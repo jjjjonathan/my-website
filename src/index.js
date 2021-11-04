4bd656748156aa1b4af269122049581d9ecef69f
jjjjonathan <hornathanjon@gmail.com>
2021-11-04 14:51:50 -0700

    remove rellax

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import './styles.css';
 import './darkModeToggle.js';
-import Rellax from 'rellax';
-
-document.addEventListener('DOMContentLoaded', () => {
-  var rellax = new Rellax('.rellax');
-});
