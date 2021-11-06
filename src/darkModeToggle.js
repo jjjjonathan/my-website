dc6692f70e1a3ce9c0dcab3cb72b494ad44fae4f
jjjjonathan <hornathanjon@gmail.com>
2021-11-05 17:35:05 -0700

    Fix dark mode toggle

--- src/darkModeToggle.js
+++ src/darkModeToggle.js
@@ -4,10 +4,18 @@ const listenForDarkMode = () => {
   const handler = (event) => {
     const htmlTag = document.querySelector('html');
 
-    if (event.target.checked === true) {
+    if (toggler.dataset.toggle === 'off') {
       htmlTag.classList.add('dark');
+
+      toggler.dataset.toggle = 'on';
+      toggler.classList.remove('bi-toggle-off');
+      toggler.classList.add('bi-toggle-on');
     } else {
       htmlTag.classList.remove('dark');
+
+      toggler.dataset.toggle = 'off';
+      toggler.classList.remove('bi-toggle-on');
+      toggler.classList.add('bi-toggle-off');
     }
   };
 
