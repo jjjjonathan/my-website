be6f60cc523eaffd88a00308f93776d23fa7c017
jjjjonathan <hornathanjon@gmail.com>
2022-01-10 15:05:07 -0800

    Add layout component for global styling

---
+++ src/components/layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: teal;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
