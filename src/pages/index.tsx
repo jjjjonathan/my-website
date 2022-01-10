bfd1b8578a03c28f70df600dc55b3230b06faf90
jjjjonathan <hornathanjon@gmail.com>
2022-01-10 15:51:30 -0800

    Switch index to TS

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <main>
+        <title>Jonathan Horn, Web Developer</title>
+        <h1>Jonathan Horn</h1>
+        <h2>Full Stack Web Developer</h2>
+      </main>
+    </Layout>
+  );
+};
+
+export default IndexPage;
