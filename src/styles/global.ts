4446f460eeabae970c8d247975580df000eff7bd
jjjjonathan <hornathanjon@gmail.com>
2022-01-10 23:57:11 -0800

    Add type scale to global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
   
   html {
     box-sizing: border-box;
+    font-size: 100%;
   }
 
   body {
@@ -17,10 +18,43 @@ const GlobalStyle = createGlobalStyle`
     color: ${(props) => props.theme.colors.red};
     font-family: 'Jost', sans-serif;
     font-weight: 800;
+    line-height: 1.75;
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  h1, h2, h3, h4, h5 {
+    margin: 3rem 0 1.38rem;
+    font-family: 'Jost', sans-serif;
+    font-weight: 800;
+    line-height: 1.3;
   }
 
   h1 {
-    
+    margin-top: 0;
+    font-size: 7.594rem;
+  }
+
+  h2 {
+    font-size: 5.063rem;
+  }
+
+  h3 {
+    font-size: 3.375rem;
+  }
+
+  h4 {
+    font-size: 2.25rem;
+  }
+
+  h5 {
+    font-size: 1.5rem;
+  }
+
+  small, .text_small {
+    font-size: 0.667rem;
   }
 `;
 
