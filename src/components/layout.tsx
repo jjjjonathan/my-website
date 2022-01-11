c341885895cc52edfa7502bca8c82d9e4709129a
jjjjonathan <hornathanjon@gmail.com>
2022-01-10 16:10:39 -0800

    Add styled-reset

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  
+  html {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
