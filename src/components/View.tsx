4e9144ae276d32e404f69ec2f60423991f6b7217
jjjjonathan <hornathanjon@gmail.com>
2022-01-11 21:49:03 -0800

    Add border to View component

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -5,20 +5,28 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Box = styled.div`
+const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  padding: 25px;
+`;
+
+const Box = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 25px;
+  border: 2px solid ${(props) => props.theme.colors.black};
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-const View = ({ children }: Props) => {
-  return (
+const View = ({ children }: Props) => (
+  <Container>
     <Box>
       <div>{children}</div>
     </Box>
-  );
-};
+  </Container>
+);
 
 export default View;
