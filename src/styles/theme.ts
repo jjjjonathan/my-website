fa9099c2439aaa70e52cdd16839bce110dfe09c2
jjjjonathan <hornathanjon@gmail.com>
2022-01-22 23:58:42 -0800

    Update white color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
   colors: {
-    white: '#fcfcfc',
+    white: '#f5f5f5',
     black: '#191923',
     red: '#ed254e',
     yellow: '#ffbe0b',
