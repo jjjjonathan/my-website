97505e9a8ce5a7d2935af694b9cd981803861f71
jjjjonathan <hornathanjon@gmail.com>
2022-02-23 18:50:15 -0600

    Add shrinking box animation

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -8,18 +8,15 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Container = styled.div`
+const Container = styled(motion.div)`
   width: 100vw;
   min-height: 100vh;
-  padding: 20px;
 `;
 
 const Box = styled(motion.div)`
   width: 100%;
   height: 100%;
   min-height: calc(100vh - 40px);
-  padding: 25px;
-  border-radius: 25px;
   background-color: ${({ theme }) => theme.colors.dark};
   display: flex;
   align-items: center;
@@ -35,33 +32,51 @@ const Box = styled(motion.div)`
 `;
 
 const View = ({ children }: Props) => {
-  const controls = useAnimation();
+  const containerControls = useAnimation();
+  const boxControls = useAnimation();
   const { ref, inView } = useInView({ threshold: 0.75 });
   const [isRotated, setIsRotated] = useState(false);
 
   useEffect(() => {
     if (inView && !isRotated) {
-      controls.start('rotate');
+      containerControls.start('partial');
+      boxControls.start('rotate');
       setIsRotated(true);
     }
-  }, [controls, inView, isRotated]);
+  }, [containerControls, boxControls, inView, isRotated]);
 
   const getRotation = () => {
     const range = Math.random() + 1.5;
     return Math.random() >= 0.5 ? range : -range;
   };
 
-  const variants = {
+  const containerVariants = {
+    full: { padding: '0px' },
+    partial: { padding: '20px' },
+  };
+
+  const boxVariants = {
     straight: { rotate: 0 },
     rotate: {
       rotate: getRotation(),
+      padding: '25px',
+      borderRadius: '25px',
       transition: { delay: 0.2, type: 'spring', bounce: 0.75 },
     },
   };
 
   return (
-    <Container>
-      <Box ref={ref} variants={variants} initial="straight" animate={controls}>
+    <Container
+      variants={containerVariants}
+      initial="full"
+      animate={containerControls}
+    >
+      <Box
+        ref={ref}
+        variants={boxVariants}
+        initial="straight"
+        animate={boxControls}
+      >
         <div>{children}</div>
       </Box>
     </Container>
