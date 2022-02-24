8166256fbf1e19191bef92d2d81b009b53f81303
jjjjonathan <hornathanjon@gmail.com>
2022-02-23 21:30:26 -0600

    Simplify shrinking box animation

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -16,7 +16,8 @@ const Container = styled(motion.div)`
 const Box = styled(motion.div)`
   width: 100%;
   height: 100%;
-  min-height: calc(100vh - 40px);
+  min-height: 100vh;
+  padding: 35px;
   background-color: ${({ theme }) => theme.colors.dark};
   display: flex;
   align-items: center;
@@ -32,45 +33,34 @@ const Box = styled(motion.div)`
 `;
 
 const View = ({ children }: Props) => {
-  const containerControls = useAnimation();
   const boxControls = useAnimation();
   const { ref, inView } = useInView({ threshold: 0.75 });
   const [isRotated, setIsRotated] = useState(false);
 
   useEffect(() => {
     if (inView && !isRotated) {
-      containerControls.start('partial');
       boxControls.start('rotate');
       setIsRotated(true);
     }
-  }, [containerControls, boxControls, inView, isRotated]);
+  }, [boxControls, inView, isRotated]);
 
   const getRotation = () => {
     const range = Math.random() + 1.5;
     return Math.random() >= 0.5 ? range : -range;
   };
 
-  const containerVariants = {
-    full: { padding: '0px' },
-    partial: { padding: '20px' },
-  };
-
   const boxVariants = {
     straight: { rotate: 0 },
     rotate: {
       rotate: getRotation(),
-      padding: '25px',
+      scale: 0.95,
       borderRadius: '25px',
       transition: { delay: 0.2, type: 'spring', bounce: 0.75 },
     },
   };
 
   return (
-    <Container
-      variants={containerVariants}
-      initial="full"
-      animate={containerControls}
-    >
+    <Container>
       <Box
         ref={ref}
         variants={boxVariants}
